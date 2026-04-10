7b65c6ee036a20bd2daeb831a7aec781ad8f2f85
Vu Anh Kiet <vuanhkiet2505@gmail.com>
2026-04-10 12:20:17 +0700

    Them Request va Response hoan thien tang Protocol

--- BTL/src/main/java/com/auction/protocol/Request.java
+++ BTL/src/main/java/com/auction/protocol/Request.java
@@ -1,1 +1,33 @@
 package com.auction.protocol;
+
+import java.io.Serializable;
+
+public class Request implements Serializable {
+    private static final long serialVersionUID=1L;
+    private MessageType type;
+    private Object payload; 
+    public Request(MessageType type) {
+        this.type = type; 
+        this.payload = null;
+    }
+    public Request(MessageType type, Object payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+    public MessageType getType() {
+        return type;
+    }
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+    public Object getPayload() {
+        return payload;
+    }
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+    
+}
--- BTL/src/main/java/com/auction/protocol/Response.java
+++ BTL/src/main/java/com/auction/protocol/Response.java
@@ -1,1 +1,56 @@
 package com.auction.protocol;
+
+import java.io.Serializable;
+
+public class Response implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private MessageType type;    // Loại phản hồi (VD: LOGIN_RESPONSE)
+    private boolean isSuccess;   // Server báo Thành công (true) hay Thất bại (false)
+    private String message;      // Lời nhắn cụ thể (VD: "Sai mật khẩu", "Đặt giá thành công")
+    private Object payload;      // Dữ liệu trả về (VD: Danh sách phiên đấu giá, Thông tin User)
+
+    public Response(MessageType type, boolean isSuccess, String message, Object payload) {
+        this.type = type;
+        this.isSuccess = isSuccess;
+        this.message = message;
+        this.payload = payload;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(boolean isSuccess) {
+        this.isSuccess = isSuccess;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+    
+}
