f26fc611ad01782bef3f760f406527400e357f75
Fliellie <fliellieofenix@gmail.com>
2026-04-17 12:25:57 +0700

    Revert "Hoan thanh gop code tu nhanh Kiet vao Dev"
    This reverts commit f006aa3aaf9354e1e89f84ae1ab49b1734e93db2, reversing
    changes made to 5bead6009c266c7e2f721d1e61a2e2132ae42058.

--- BTL/src/main/java/com/auction/server/service/AuctionServer.java
+++ BTL/src/main/java/com/auction/server/service/AuctionServer.java
@@ -1,31 +1,23 @@
 package com.auction.server.service;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+
+
+// toàn bộ code trong file này chỉ là thử nghiệm và có thể xóa đi----------------------------------
+
+// yêu cầu viết lại logic
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+
 
 public class AuctionServer {
-    // Cổng PORT mặc địch để Server mở cửa đón khách
-    private static final int PORT=8000;
-    public static void main(String[] args) {
-        System.out.println("=== He thong may chu BIDDING dang khoi dong ===");            
-        try (ServerSocket serverSocket=new ServerSocket(PORT)) {
-            // ServerSocket là lớp được dùng để tạo server lắng nghe các kết nối từ phía Client 
-            System.out.println("Server dang lang nghe ket noi tai cong " + PORT);
-            // Vong lap vo tan de servrer luon bat va chao don nguoi dung
-            while (true) {
-                // Lệnh accept() sẽ chặn luồng lại ở đây cho đến khi có một Client kết nối tới
-                Socket clienSocket=serverSocket.accept();
-                System.out.println("Co mot nguoi choi vua ket noi tu IP: " + clienSocket.getInetAddress());
-                // Tao nhan vien xu ly va cho chay luong 
-                ClientHandler handler=new ClientHandler(clienSocket);
-                new Thread(handler).start();
-                 
-            }
-        }
-        catch (IOException e) {
-            System.err.println("Errol: Loi he thong");
-        }
 
+    public static boolean checkLogin(String email, String password) {
+        // Giả sử logic kiểm tra đơn giản
+        if (email.contains("@") && password.length() >= 6) {
+            return true;
+        }
+        return false;
     }
 }
