4cb46b5a44c09f5bd849ef9cd4aa936979ba3979
Fliellie <fliellieofenix@gmail.com>
2026-04-23 10:22:19 +0700

    cap nhat tran man hinh

--- BTL/src/main/java/com/auction/client/AuctionClient.java
+++ BTL/src/main/java/com/auction/client/AuctionClient.java
@@ -1,23 +1,30 @@
 package com.auction.client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AuctionClient extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // Nạp file fxml
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/auction/client/view/login.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setX(0);
-        stage.setY(0);
-        stage.setTitle("JavaFX Button Event");
+        // Get screen size
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+
+        // Create the scene with the screen dimensions
+        Scene scene = new Scene(fxmlLoader.load(), screenBounds.getWidth(), screenBounds.getHeight());
+
+        stage.setTitle("Auction");
         stage.setScene(scene);
+
+        // Force the window to be maximized and stay within screen bounds
         stage.setMaximized(true);
         stage.show();
+
 
     }
 
