8e4168edb6ffd6928a61ad5ff2e26639a395e79d
Vu Anh Kiet <vuanhkiet2505@gmail.com>
2026-05-14 23:40:22 +0700

    test lan 1

--- BTL/src/main/java/com/auction/protocol/MessageType.java
+++ BTL/src/main/java/com/auction/protocol/MessageType.java
@@ -31,6 +31,18 @@ public class MessageType {
     // Server thông báo với client(Dùng sau khi có người vừa đăng bán thành công)
     public static final String UPDATE_AUCTION_LIST_RESPONSE = "UPDATE_AUCTION_LIST_RESPONSE";
 
+    // 1. Client xin danh sách các phiên đấu giá đang diễn ra
+    public static final String GET_ACTIVE_AUCTIONS_REQUEST = "GET_ACTIVE_AUCTIONS_REQUEST";
+    public static final String GET_ACTIVE_AUCTIONS_RESPONSE = "GET_ACTIVE_AUCTIONS_RESPONSE";
 
-    // (Sau này làm tính năng gì thì cứ khai báo thêm vào đây)
+    // 2. Server broadcast khi có 1 phiên đấu giá mới vừa được Shop đưa lên sàn
+    public static final String UPDATE_ACTIVE_AUCTIONS_RESPONSE = "UPDATE_ACTIVE_AUCTIONS_RESPONSE";
+
+    // 3. Client gửi lệnh xin ra giá (Bid)
+    public static final String PLACE_BID_REQUEST = "PLACE_BID_REQUEST";
+    public static final String PLACE_BID_RESPONSE = "PLACE_BID_RESPONSE";
+
+    // 4. Server broadcast cho cả sàn biết vừa có giá mới (Đúng chuẩn Giao kèo 2 của Hùng)
+    public static final String BROADCAST_NEW_BID = "BROADCAST_NEW_BID";
 }
+    // (Sau này làm tính năng gì thì cứ khai báo thêm vào đây)
---
+++ BTL/src/main/java/com/auction/server/handler/GetActiveAuctionsHandler.java
@@ -0,0 +1,39 @@
+package com.auction.server.handler;
+
+import com.auction.protocol.MessageType;
+import com.auction.common.model.auction.Auction;
+import com.auction.protocol.Response;
+import com.auction.server.annotation.CommandMap;
+import com.auction.server.model.ServerContext;
+import com.google.gson.Gson;
+import org.java_websocket.WebSocket;
+
+import java.util.List;
+import java.util.Map;
+
+// 🚀 Đã gọi chuẩn từ MessageType
+@CommandMap(value = MessageType.GET_ACTIVE_AUCTIONS_REQUEST)
+public class GetActiveAuctionsHandler implements IMessageHandler {
+
+    @Override
+    public void handle(WebSocket conn, Map<String, Object> data, Gson gson, ServerContext context) {
+        try {
+            // Móc danh sách Phiên đấu giá từ RAM ra
+            List<Auction> activeAuctions = context.getActiveAuctions();
+
+            // Đóng gói trả về cho Client vừa gửi Request
+            Response response = new Response(MessageType.GET_ACTIVE_AUCTIONS_RESPONSE, "SUCCESS", "Lấy danh sách đấu giá thành công!");
+            response.getData().put("auctionList", activeAuctions);
+
+            // Chỉ gửi trả lại cho ĐÚNG cái thằng vừa xin
+            conn.send(gson.toJson(response));
+
+            System.out.println("-> [GetActiveAuctions] Đã gửi " + activeAuctions.size() + " phiên đấu giá cho 1 Client.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Response errResponse = new Response(MessageType.GET_ACTIVE_AUCTIONS_RESPONSE, "ERROR", "Lỗi server khi lấy danh sách: " + e.getMessage());
+            conn.send(gson.toJson(errResponse));
+        }
+    }
+}
